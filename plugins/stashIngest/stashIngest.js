3ff2d04062101b4ec8cdfa6f3d4b7e132498b74d
jsmthy <jsmthy@protonmail.com>
2025-03-31 23:56:02 -0400

    Simplify main function by removing unnecessary wrapping and retaining logging functionality

--- plugins/stashIngest/stashIngest.js
+++ plugins/stashIngest/stashIngest.js
@@ -1,11 +1,9 @@
 (function () {
   'use strict';
 
-  function main() {
+  log.Info(`Stash Ingest Plugin Loaded`)
+  log.Info(`Input: `,input);
 
-    log.Info(`Stash Ingest Plugin Loaded`)
-    log.Info(`Input: `,input);
-  
-    return { Output: "ok" };
-  }
+  return { Output: "ok" };
+
 })();
