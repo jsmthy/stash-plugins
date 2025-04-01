ce6d26345cfaa2f53561d39d9156a45286b5a326
jsmthy <jsmthy@protonmail.com>
2025-04-01 00:06:08 -0400

    Improve logging in stashIngest.js by serializing input1 for better clarity

--- plugins/stashIngest/stashIngest.js
+++ plugins/stashIngest/stashIngest.js
@@ -2,8 +2,8 @@
   'use strict';
 
   log.Debug(`Stash Ingest Plugin Loaded`)
-  log.Debug(`Input1: `,`test123`);
-  log.Debug(input);
+  log.Debug(`Input1: `);
+  log.Debug(JSON.stringify(input1));
   log.Debug(`Input2: `);
   log.Debug(`Input3: `);
   log.Debug(`Input4: `);
