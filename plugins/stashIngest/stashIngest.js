9fd0776bc2ea0c6569b76b4129c6e921dd97fc6f
jsmthy <jsmthy@protonmail.com>
2025-04-01 00:02:12 -0400

    Enhance logging in stashIngest.js to provide detailed input information

--- plugins/stashIngest/stashIngest.js
+++ plugins/stashIngest/stashIngest.js
@@ -2,7 +2,10 @@
   'use strict';
 
   log.Info(`Stash Ingest Plugin Loaded`)
-  log.Info(`Input: `,JSON.stringify(input));
+  log.Info(`Input1: `,input);
+  log.Info(`Input2: `,input.Args);
+  log.Info(`Input3: `,input.Args.hookContext);
+  log.Info(`Input4: `,input.Args.hookContext.type);
 
   return { Output: "ok" };
 
